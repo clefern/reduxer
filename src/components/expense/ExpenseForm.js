f06e73305b0f9bf9bf9dba56ae37a85326cf208a
Cleber Fernandes <cleber_sim@outlook.com>
2022-02-21 21:51:09 -0300

    applying redux

--- src/components/expense/ExpenseForm.js
+++ src/components/expense/ExpenseForm.js
@@ -1,20 +1,37 @@
 import React, { useState } from "react"
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+const date = new Date();
+const now = moment();
 
 const ExpenseForm = () => {
     const [description, setDescription] = useState('');
     const [amount, setAmount] = useState('');
     const [note, setNote] = useState('');
+    const [createdAt, setCreatedAt] = useState(null);
+    const [dateFocus, setDateFocus] = useState(false);
     const onDescriptionChange = (e) => {
-        console.log(e.target.value);
-        // setDescription(e.target.value)
+        setDescription(e.target.value)
     }
     const onAmountChange = (e) => {
+        const amountVal = e.target.value;
+        if (amountVal.match(/^\d*(\.\d{0,2})?$/)) {
+            setAmount(e.target.value)
+        }
         console.log(e.target.value);
-        // setAmount(e.target.value)
     }
     const onNoteChange = (e) => {
-        console.log(e.target.value);
-        // setNote(e.target.value)
+        setNote(e.target.value)
+    }
+    const onChangeDate = (e) => {
+        console.log(e);
+        // setCreatedAt(e.target.value)
+    }
+    const onDateFocusChange = (e) => {
+        console.log(e);
+        // setDate(e.target.value)
     }
     return (
         <div>
@@ -40,6 +57,13 @@ const ExpenseForm = () => {
                     value={note}
                     onChange={onNoteChange}
                 ></textarea>
+
+                <SingleDatePicker
+                    date={createdAt}
+                    onDateChange={onChangeDate}
+                    focused={dateFocus}
+                    onFocusChange={onDateFocusChange}
+                />
 
                 <button>Add Expense</button>
             </form>
